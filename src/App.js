171ecad500f949eb2941e7bba0fe6e5f06bcf214
Rhys Smoker <r.smoker@gmail.com>
2024-04-23 15:50:27 -0700

    changed image source to try to better match github pages

--- src/App.js
+++ src/App.js
@@ -126,7 +126,7 @@ function App() {
 			<header className="App-header">
 				<div className="icon-title-container">
 					<img
-						src="/logo.webp"
+						src="nounsofassembly/logo.webp"
 						className="App-logo"
 						alt="logo"
 						style={{ width: "200px", height: "auto" }}
